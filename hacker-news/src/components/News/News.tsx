72e929c2e9439d434dc622852128edf2a7a0bd84
Kristute <r.kryste@gmail.com>
2023-01-16 11:35:29 +0200

    Add Pagination component

--- hacker-news/src/components/News/News.tsx
+++ hacker-news/src/components/News/News.tsx
@@ -1,7 +1,8 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Grid } from "@mui/material";
 
 import useApiRequest from "../../hooks/useApiRequest/useApiRequest";
+import PaginationItem from "../Pagination";
 import Story from "../Story/Story";
 import ErrorHandler from "../ErrorHandler";
 
@@ -10,8 +11,32 @@ interface NewsData {
 }
 
 const News = () => {
-  const LIMIT = 50;
-  const API = `https://hacker-news.firebaseio.com/v0/newstories.json?&orderBy="$key"&startAt="${LIMIT}"&endAt="80"`;
+  const pageNumberLimit = 5;
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [maxPageLimit, setMaxPageLimit] = useState<number>(5);
+  const [minPageLimit, setMinPageLimit] = useState<number>(0);
+    
+  const onPageChange= (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  }
+
+  const onPrevClick = () => {
+    if((currentPage-1) % pageNumberLimit === 0){
+        setMaxPageLimit(maxPageLimit - pageNumberLimit);
+        setMinPageLimit(minPageLimit - pageNumberLimit);
+    }
+    setCurrentPage(prev=> prev-1);
+   }
+
+  const onNextClick = () => {
+      if(currentPage+1 > maxPageLimit){
+          setMaxPageLimit(maxPageLimit + pageNumberLimit);
+          setMinPageLimit(minPageLimit + pageNumberLimit);
+      }
+      setCurrentPage(prev=>prev+1);
+    }
+
+  const API = `https://hacker-news.firebaseio.com/v0/newstories.json?&orderBy="$key"&startAt="${currentPage}"&endAt="80"`;
 
   const { error, loading, data } = useApiRequest<NewsData>(API);
 
@@ -19,6 +44,14 @@ const News = () => {
     () => (data ? (Object.values(data) as Array<number>) : []),
     [data]
   );
+
+
+  const paginationAttributes = {
+    currentPage,
+    maxPageLimit,
+    minPageLimit,
+    response: stories,
+  };
 
   if (error) {
     return <ErrorHandler message={error?.message} />;
@@ -30,6 +63,11 @@ const News = () => {
 
   return (
     <Grid container spacing={2} sx={{ marginTop: 2 }}>
+      <PaginationItem {...paginationAttributes} 
+                  onPrevClick={onPrevClick} 
+                  onNextClick={onNextClick}
+                  onPageChange={onPageChange}
+      />
       <Grid item sx={{ width: "100%" }}>
         {stories.map((item: number) => {
           return <Story key={item} item={item} />;
---
+++ hacker-news/src/components/Pagination.tsx
@@ -0,0 +1,41 @@
+import { Pagination } from "@mui/material";
+import { ChangeEvent } from "react";
+
+interface paginationAttributes {
+    currentPage: number;
+    maxPageLimit: number;
+    minPageLimit: number;
+    response: number[];
+    onPrevClick: any;
+    onNextClick: any;
+    onPageChange: any;
+}
+  
+
+const PaginationItem = (props: paginationAttributes) => {
+    const { currentPage} = props;
+    const statTotalPages = 50;
+    const totalPages = statTotalPages-1;
+  
+    
+      const pages = [];
+      for(let i=1 ; i<=totalPages; i++){
+          pages.push(i);
+      }
+
+      const handlePageClick = (event: ChangeEvent<unknown>, value: number) => {
+            props.onPageChange(Number(value))      
+          };
+  
+      return (
+          <div className="main">              
+              <Pagination 
+                  count={pages.length} 
+                  page={currentPage} 
+                  onChange={handlePageClick} 
+                />
+          </div>
+      )
+  }
+  
+  export default PaginationItem;
