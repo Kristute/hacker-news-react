402cf3f3b5ab825cda1749f3665ff588c8c888a8
Kristute <r.kryste@gmail.com>
2023-01-30 15:48:24 +0200

    Add types to arrays, check if not loading

--- hacker-news/src/hooks/usePagination/usePagination.ts
+++ hacker-news/src/hooks/usePagination/usePagination.ts
@@ -12,25 +12,23 @@ interface paginationAttributes {
 const usePagination = (URL: string, newsPerPage: number) => {
   const location = useLocation();
   // data - to count total pages
-  const { data, error, loading } = useApiRequest<[]>(URL);
+  const { data, error, loading } = useApiRequest<number[]>(URL);
   const params = new URLSearchParams(location.search);
   const currentPage = Number(params.get("page")) || 1;
   // stories - to get paginated data
-  const [stories, setStories] = useState([]);
+  const [stories, setStories] = useState<number[]>([]);
 
-  const totalPages = useMemo(() => { 
-    if (data !== undefined) {
-      return (
-        Math.round(Object.entries(data as number[]).length / newsPerPage)
-      );
+  const totalPages = useMemo(() => {
+    if (!loading && data !== undefined) {
+      return Math.round(Object.entries(data as number[]).length / newsPerPage);
     }
-  }, [data, newsPerPage]);
+  }, [loading, data, newsPerPage]);
 
   const [startIndex, endIndex] = useMemo(() => {
-    return ([
+    return [
       currentPage * newsPerPage - newsPerPage,
       currentPage * newsPerPage - 1,
-    ]);
+    ];
   }, [currentPage, newsPerPage]);
 
   // TODO: move this logic from usePagination
@@ -42,11 +40,15 @@ const usePagination = (URL: string, newsPerPage: number) => {
       .then((retrievedData) => setStories(retrievedData));
   }, [startIndex, endIndex]);
 
-  const paginationAttributes = useMemo(() => ({
-    pages: totalPages,
-    stories: stories,
-    currentPage: currentPage,
-  }) as paginationAttributes, [totalPages, stories, currentPage]);
+  const paginationAttributes = useMemo(
+    () =>
+      ({
+        pages: totalPages,
+        stories: stories,
+        currentPage: currentPage,
+      } as paginationAttributes),
+    [totalPages, stories, currentPage]
+  );
 
   return {
     error,
